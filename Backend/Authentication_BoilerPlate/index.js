9587677daca4d2206147c96228ec521b6baff44e
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-04-24 12:00:58 +0530

    Add: Routes and Controller

--- Backend/Authentication_BoilerPlate/index.js
+++ Backend/Authentication_BoilerPlate/index.js
@@ -18,6 +18,8 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 4000;
 
--- Backend/Fullstack/index.js
+++ Backend/Fullstack/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://127.0.0.1:3000",
     credentials: true,
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
     allowedHeaders: ['Content-Type', 'Authorization']
