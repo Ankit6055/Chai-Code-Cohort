da297cf7116cb123a1e04a5e5435c602fe31f998
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-04-26 16:46:31 +0530

    Update: Add mail and crypto

--- Backend/Authentication_BoilerPlate/controller/user.controller.js
+++ Backend/Authentication_BoilerPlate/controller/user.controller.js
@@ -1,3 +1,10 @@
+import User from "../model/user.model.js";
+import crypto from "crypto";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const registerUser = async (req, res) => {
   // get data
   // validate
@@ -8,14 +15,75 @@ const registerUser = async (req, res) => {
   // send token as email to user
   // send success status to user
 
-  res.send("registered!")
+  // console.log(req.body);
 
-  // const { name, email, password } = req.body;
-  // if (!name || !email || !password) {
-  //   return res.status(400).json({
-  //     message: "All fields are required",
-  //   });
-  // }
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      message: "All fields are required!",
+    });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists!",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
+    console.log(user);
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not registered",
+      });
+    }
+
+    const token = crypto.randomBytes(32).toString("hex");
+    console.log(token);
+    user.verificationToken = token;
+
+    await user.save();
+
+    // Send Email
+    const transporter = nodemailer.createTransport({
+      host: "process.env.MAILTRAP_HOST",
+      port: process.env.PORT,
+      secure: false, // true for port 465, false for other ports
+      auth: {
+        user: "process.env.MAILTRAP_USERNAME",
+        pass: "process.env.MAILTRAP_PASSWORD",
+      },
+    });
+
+    const mailOption = {
+      from: process.env.MAILTRAP_SENDERMAIL,
+      to: "user.email",
+      subject: "Verify your email",
+      text: `Please click on the following link:
+      ${process.env.BASE_URL}/api/v1/users/verify/${token}
+      `,
+    };
+
+    await transporter.sendMail(mailOption);
+
+    res.status(201).json({
+      message: "User registered Successfully!",
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "User not registered!",
+      error,
+      success: false,
+    });
+  }
 };
 
 export { registerUser };
